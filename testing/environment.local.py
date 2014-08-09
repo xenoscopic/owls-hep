d1c54ba3c8a8914f64f48d225fe047e67bc2f7cc
Jacob Howard <jacob@havoc.io>
2014-08-09 18:22:09 +0100

    Fixed environment loading unit test.

--- testing/environment.local.py
+++ testing/environment.local.py
@@ -2,20 +2,18 @@
 from tempfile import mkdtemp
 
 # owls-cache imports
-from owls_cache.persistent import set_persistent_cache
 from owls_cache.persistent.caches.redis import RedisPersistentCache
 
 # owls-parallel imports
-from owls_parallel import set_parallelization_backend
 from owls_parallel.backends.batch import BatchParallelizationBackend, \
     qsub_submit, qsub_monitor
 
 
 # Create redis cache
-set_persistent_cache(RedisPersistentCache())
+persistent_cache = RedisPersistentCache()
 
 
 # Create batch parallelization backend
-set_parallelization_backend(BatchParallelizationBackend(mkdtemp(),
-                                                        qsub_submit,
-                                                        qsub_monitor))
+parallelization_backend = BatchParallelizationBackend(mkdtemp(),
+                                                      qsub_submit,
+                                                      qsub_monitor)
--- testing/test_environment.py
+++ testing/test_environment.py
@@ -3,11 +3,9 @@ import unittest
 from os.path import join, dirname
 
 # owls-cache imports
-from owls_cache.persistent import get_persistent_cache
 from owls_cache.persistent.caches.redis import RedisPersistentCache
 
 # owls-parallel imports
-from owls_parallel import get_parallelization_backend
 from owls_parallel.backends.batch import BatchParallelizationBackend
 
 # owls-hep imports
@@ -21,11 +19,11 @@ config_path = join(dirname(__file__), 'environment.py')
 class TestLoading(unittest.TestCase):
     def test(self):
         # Load the environment
-        load(config_path)
+        persistent_cache, parallelization_backend = load(config_path)
 
         # Make sure it is correct
-        self.assertIsInstance(get_persistent_cache(), RedisPersistentCache)
-        self.assertIsInstance(get_parallelization_backend(),
+        self.assertIsInstance(persistent_cache, RedisPersistentCache)
+        self.assertIsInstance(parallelization_backend,
                               BatchParallelizationBackend)
 
 
